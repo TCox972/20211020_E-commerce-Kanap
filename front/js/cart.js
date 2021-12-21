3d8f998112bdc0bcdb9e240f83a39aaa21544e2c
JORNAT <jornat.jerome@gmail.com>
2021-12-21 06:00:58 -0400

    Tableau cart terminé - début fonction modification

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,14 +7,14 @@ for (let productId in panier) {
         console.log(panier[productId])
         const idProduct = panier[productId]
 
-        document.querySelector("#cart__items").innerHTML += `<article class="cart__item" data-id="${idProduct._id}">
+        document.querySelector("#cart__items").innerHTML += `<article class="cart__item" data-id="${productId}">
         <div class="cart__item__img">
           <img src="${idProduct.image}" alt="Photographie d'un canapé">
         </div>
         <div class="cart__item__content">
           <div class="cart__item__content__titlePrice">
             <h2>${idProduct.name}</h2>
-            <h2>${idProduct.quantity}</h2>
+            <h2>${color}</h2>
             <p>${idProduct.price} €</p>
           </div>
           <div class="cart__item__content__settings">
@@ -30,3 +30,33 @@ for (let productId in panier) {
       </article>`
     }
 }
+
+function displayTotal(){
+  let panier = JSON.parse(localStorage.panier);
+
+  let amountTotal =  0;
+  let nbArticles = 0;
+
+for (let productId in panier) {
+    for(let color in panier[productId].quantity) {
+      const idProduct = panier[productId]
+      nbArticles += idProduct.quantity[color];
+      amountTotal += idProduct.quantity[color] * idProduct.price;
+    }
+  }
+  document.querySelector("#totalPrice")
+    .innerHTML = amountTotal;
+  document.querySelector("#totalQuantity")
+    .innerHTML = nbArticles;
+}
+displayTotal();
+
+function modifyQuantity(){
+  var test = document.querySelector(".itemQuantity")
+    .closest("article")
+    .dataset.id;
+
+  return console.log(test);
+}
+
+modifyQuantity();
