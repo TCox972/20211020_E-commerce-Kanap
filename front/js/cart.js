5ee3af8d242d1ab8521375a318323c142d569308
JORNAT <jornat.jerome@gmail.com>
2022-01-06 13:56:15 -0400

    Premiere ébauche suppression produit panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,13 @@
-
-
 let panier = JSON.parse(localStorage.panier);
 
-for (let productId in panier) {
-    for(let color in panier[productId].quantity) {
-        console.log(panier[productId])
-        const idProduct = panier[productId]
+// ---------------- Récupération des éléments du localstorage et attribution à la page panier
 
-        document.querySelector("#cart__items").innerHTML += `<article class="cart__item" data-id="${productId}">
+for (let productId in panier) {
+  for (let color in panier[productId].quantity) {
+    console.log(panier[productId])
+    const idProduct = panier[productId]
+
+    document.querySelector("#cart__items").innerHTML += `<article class="cart__item" data-id="${productId}">
         <div class="cart__item__img">
           <img src="${idProduct.image}" alt="Photographie d'un canapé">
         </div>
@@ -28,17 +28,19 @@ for (let productId in panier) {
           </div>
         </div>
       </article>`
-    }
+  }
 }
 
-function displayTotal(){
+// ---------------- Total panier
+
+function displayTotal() {
   let panier = JSON.parse(localStorage.panier);
 
-  let amountTotal =  0;
+  let amountTotal = 0;
   let nbArticles = 0;
 
-for (let productId in panier) {
-    for(let color in panier[productId].quantity) {
+  for (let productId in panier) {
+    for (let color in panier[productId].quantity) {
       const idProduct = panier[productId]
       nbArticles += idProduct.quantity[color];
       amountTotal += idProduct.quantity[color] * idProduct.price;
@@ -51,12 +53,33 @@ for (let productId in panier) {
 }
 displayTotal();
 
-function modifyQuantity(){
+// ----------------- Modification de la quantité dans le panier
+
+function modifyQuantity() {
   var test = document.querySelector(".itemQuantity")
     .closest("article")
     .dataset.id;
-
-  return console.log(test);
 }
 
 modifyQuantity();
+displayTotal();
+
+// ---------------- Suppression d'un élément du panier
+
+var supprBtn = document.querySelectorAll(".deleteItem");
+console.log(supprBtn)
+
+for (i = 0; i < supprBtn.length; i++) {
+  supprBtn[i]
+    .addEventListener("click", (event) => {
+      event.preventDefault;
+      event.stopPropagation;
+
+      elToDelete = 
+
+      console.log(elToDelete)
+
+    })
+}
+
+
