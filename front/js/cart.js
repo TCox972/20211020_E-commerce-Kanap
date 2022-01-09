24f6530776c3cff09f14c2273c781e1b97209322
JORNAT <jornat.jerome@gmail.com>
2022-01-09 16:02:58 -0400

    Bouton suppression actif + actualisation localStorage

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,14 @@
-let panier = JSON.parse(localStorage.panier);
+
+let panier = JSON.parse(localStorage.getItem("panier"));
+console.log(panier)
 
 // ---------------- Récupération des éléments du localstorage et attribution à la page panier
 
 for (let productId in panier) {
   for (let color in panier[productId].quantity) {
-    console.log(panier[productId])
     const idProduct = panier[productId]
 
-    document.querySelector("#cart__items").innerHTML += `<article class="cart__item" data-id="${productId}">
+    document.querySelector("#cart__items").innerHTML += `<article class="cart__item" data-color="${color}" data-id="${productId}">
         <div class="cart__item__img">
           <img src="${idProduct.image}" alt="Photographie d'un canapé">
         </div>
@@ -30,6 +31,8 @@ for (let productId in panier) {
       </article>`
   }
 }
+
+
 
 // ---------------- Total panier
 
@@ -67,19 +70,29 @@ displayTotal();
 // ---------------- Suppression d'un élément du panier
 
 var supprBtn = document.querySelectorAll(".deleteItem");
-console.log(supprBtn)
 
-for (i = 0; i < supprBtn.length; i++) {
+for (let i = 0; i < supprBtn.length; i++) {
+
   supprBtn[i]
     .addEventListener("click", (event) => {
       event.preventDefault;
       event.stopPropagation;
 
-      elToDelete = 
+      var idDelete = supprBtn[i].closest("article").dataset.id
+      var colorDelete = supprBtn[i].closest("article").dataset.color
+      
+      let panier = JSON.parse(localStorage.getItem("panier"))
+      delete panier[idDelete].quantity[colorDelete]
+      
+      if (panier[idDelete].quantity == false) delete panier[idDelete]
 
-      console.log(elToDelete)
-
+      supprBtn[i].closest("article").remove()
+      localStorage.setItem("panier", JSON.stringify(panier));
+      displayTotal()
     })
-}
+  }
+
+console.log(panier)
+
 
 
